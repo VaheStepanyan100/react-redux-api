6ba496cd90c7b64140ae86918d5a2bbeb727ad71
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-06-20 19:45:24 +0400

    Import and wrap <Provider> and inject store

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
 function App() {
-  return <h1>hello</h1>;
+  return (
+    <Provider store={store}>
+      
+    </Provider>
+  );
 }
 
 export default App;
